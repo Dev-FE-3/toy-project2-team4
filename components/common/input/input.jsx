e90aee7bff660e67da63fcf34473192eaeae210a
Jang Eunhye <hh961130@naver.com>
2025-02-26 15:51:49 +0900

    comment: 주석 변경 (-> JSDoc )

--- components/common/input/input.jsx
+++ components/common/input/input.jsx
@@ -2,6 +2,18 @@
 import { useState } from "react";
 import styles from "./input.module.css";
 
+/**
+ * Input 컴포넌트 props
+ * 
+ * @param { "text" | "password" | "time" | "date" } type - 입력 타입 (text, password, time, date 중 하나)
+ * @param { string } placeholder - 입력란에 표시될 텍스트
+ * @param { string } defaultValue - 기본값
+ * @param { number } maxLength - 입력할 수 있는 최대 문자 길이
+ * @param { boolean } isWrong - 오류 상태 여부 (true/false)
+ * @param { boolean } isDisabled - 입력란 비활성화 여부 (true/false)
+ * @param { (value: string) => void } onChange - 값 변경 시 호출될 함수 (상태 변경 함수)
+ */
+
 export default function Input({
   type = "text",
   placeholder = "",
@@ -27,9 +39,3 @@ export default function Input({
     ></input>
   );
 }
-
-// type: "text", ""password", "time", "date"
-// placeholder : string
-// maxLength : {number}
-// isWrong : {boolean}
-// isDisabled : {boolean}
