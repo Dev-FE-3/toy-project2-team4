1a332712dd049d20591a2c2046cb43229f30fb43
hgTime <hgtime@gmail.com>
2025-03-01 14:11:32 +0900

    feat: 공통 드롭박스 컴포넌트 완성

--- app/modificationHistory/page.jsx
+++ app/modificationHistory/page.jsx
@@ -1,24 +1,30 @@
 "use client";
 
-// import Button from "@components/common/button/button";
+import Button from "../../components/common/button/button";
+import Dropdown from "../../components/common/dropdown/dropdown";
 
 const modificationHistory = () => {
+  const options = ["1번 옵션입니다.", "2번 옵션", "3번 옵션임"];
+
   return (
     <>
       <div>정정 신청 내역 확인 페이지</div>
-      {/* <Button color="blue" onClick={(e) => console.log(e.target.dataset.num)} data-num="test1">
-        확인
-      </Button>
-      <Button color="red" onClick={() => console.log("취소 버튼 클릭함")}>
-        취소
-      </Button>
-      <Button color="yellow" onClick={() => console.log("정정 버튼 클릭함")}>
-        정정
-      </Button>
-      <Button color="gray" onClick={() => console.log("삭제 버튼 클릭함")}>
-        삭제
-      </Button> */}
+      <Dropdown initialOptions={options} />
+      <div>
+        <Button color="blue" onClick={(e) => console.log(e.target.dataset.num)} data-num="test1">
+          확인
+        </Button>
+        <Button color="red" onClick={() => console.log("취소 버튼 클릭함")}>
+          취소
+        </Button>
+        <Button color="yellow" onClick={() => console.log("정정 버튼 클릭함")}>
+          정정
+        </Button>
+        <Button color="gray" onClick={() => console.log("삭제 버튼 클릭함")}>
+          삭제
+        </Button>
+      </div>
     </>
   );
 };
-export default modificationHistory;
+export default modificationHistory; 
---
+++ components/common/dropdown/dropdown.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useRef, useEffect } from "react";
+import styles from "./dropdown.module.scss";
+import Icon from "../icon/icon";
+
+/**
+ * @param {string[]} initialOptions - 드롭다운 옵션 리스트
+ * @param {function} onSelect - 옵션 선택 시 실행할 콜백 함수 (선택된 값 전달)
+ */
+
+const Dropdown = ({ initialOptions = [], onSelect}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectOption, setSelectOption] = useState('선택하기')
+  const dropdownRef = useRef(null);
+
+  const handleClickOutside = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  const handleItemClick = (option) => {
+    setSelectOption(option)
+    if (onSelect) onSelect(option); // 부모 컴포넌트로 선택 값 전달
+    setIsOpen(false);  
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div ref={dropdownRef} className={styles.dropdownContainer}>
+      <button onClick={() => setIsOpen((prev) => !prev)} className={styles.dropdown}>
+        {selectOption}
+        <Icon style="rounded" iconname="stat_minus_1" size="2rem" />
+      </button>
+      <div>
+        {isOpen && (
+          <div className={styles.dropdownList}>
+            <ul>
+              {initialOptions.map((option, index) => (
+                <li key={index} className={styles.dropItem} onClick={() => {handleItemClick(option)}}>
+                  {option}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
