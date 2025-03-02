2b23ea632f74e824b8ac5740679c86fa804828c7
hgTime <hgtime@gmail.com>
2025-03-03 00:22:10 +0900

    feat: 초기 마크업

--- app/modificationHistory/page.jsx
+++ app/modificationHistory/page.jsx
@@ -1,24 +1,70 @@
 "use client";
-
-// import Button from "@components/common/button/button";
+// import { useState } from "react";
+import Button from "../../components/common/button/button";
 
 const modificationHistory = () => {
+  // const [options, setOption] = useState([])
+  const datas = [
+    {
+      id: 16,
+      date: "2025.02.20",
+      reason: "업무 연장",
+      content: "추가 수업한 부분에 대해서 정정을 신청...",
+      status: "대기",
+    },
+    {
+      id: 15,
+      date: "2024.02.20",
+      reason: "업무 연장",
+      content: "추가 수업한 부분에 대해서 정정을 신청...",
+      status: "대기",
+    },
+    {
+      id: 14,
+      date: "2023.02.20",
+      reason: "업무 연장",
+      content: "추가 수업한 부분에 대해서 정정을 신청...",
+      status: "정정 완료",
+    },
+  ];
   return (
-    <>
-      <div>정정 신청 내역 확인 페이지</div>
-      {/* <Button color="blue" onClick={(e) => console.log(e.target.dataset.num)} data-num="test1">
-        확인
-      </Button>
-      <Button color="red" onClick={() => console.log("취소 버튼 클릭함")}>
-        취소
-      </Button>
-      <Button color="yellow" onClick={() => console.log("정정 버튼 클릭함")}>
-        정정
-      </Button>
-      <Button color="gray" onClick={() => console.log("삭제 버튼 클릭함")}>
-        삭제
-      </Button> */}
-    </>
+    <main className="">
+      <p>
+        총 <strong>{datas.length}</strong> 개
+      </p>
+
+      {datas.length === 0 ? (
+        <p className="noData">정정 신청 내역이 없습니다.</p>
+      ) : (
+        <table>
+          <thead className="listTitle">
+            <tr>
+              <th>번호</th>
+              <th>날짜</th>
+              <th>내용</th>
+              <th>사유</th>
+              <th>상태</th>
+              <th>취소</th>
+            </tr>
+          </thead>
+          <tbody>
+            {datas.map((data) => (
+              <tr key={data.id}>
+                <td>{data.id}</td>
+                <td>{data.date}</td>
+                <td>{data.reason}</td>
+                <td>{data.content}</td>
+                <td>{data.status}</td>
+                <td>
+                  <Button color="red">삭제</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </main>
   );
 };
 export default modificationHistory;
+
