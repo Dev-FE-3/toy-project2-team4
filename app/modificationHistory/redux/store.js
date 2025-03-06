228165792e198abf96ab3dc418aa14370cca052e
hgTime <hgtime@gmail.com>
2025-03-07 08:56:00 +0900

    feat: 서버에서 redux devtools 실행되지 않도록 함

--- app/modificationHistory/redux/store.js
+++ app/modificationHistory/redux/store.js
@@ -2,7 +2,11 @@ import { createStore } from 'redux';
 import counterReducer from './counterReducer';
 
 // createStore로 스토어를 생성합니다
-const store = createStore(counterReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(
+  counterReducer,
+  typeof window !== "undefined" && window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : undefined,
 );
 
 export default store; 
