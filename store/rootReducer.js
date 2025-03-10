9d19520bbfb9a217a70f15894abfa331808d1cfa
Jang Eunhye <hh961130@naver.com>
2025-03-10 15:38:59 +0900

    style: 수업확인 페이지 리듀서 파일명 변경

--- store/rootReducer.js
+++ store/rootReducer.js
@@ -1,14 +1,14 @@
 import { combineReducers } from "redux";
 import counterReducer from "./reducers/counterReducer";
 import authReducer from "./reducers/authReducer";
-import calendarReducer from "./reducers/calendarReducer";
+import classCalendarReducer from "./reducers/classCalendarReducer";
 import modificationHistoryReducer from "./reducers/modificationHistoryReducer";
 import paymentHistoryReducer from "./reducers/paymentHistoryReducer";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   auth: authReducer,
-  calendar: calendarReducer,
+  classCalendar: classCalendarReducer,
   modificationHistory: modificationHistoryReducer,
   paymentHistory: paymentHistoryReducer,
 });
