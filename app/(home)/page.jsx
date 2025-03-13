39c6ca388b0d32002d3a133a216835cc03138cbe
Jang Eunhye <hh961130@naver.com>
2025-03-13 19:04:15 +0900

    refactor: 로컬스토리지 적용

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -5,7 +5,6 @@ import {
   CalendarDate,
   CalendarHeader,
   CLASS_TITLES,
- // classData,
   INSTRUCTORS,
 } from "../../components/classCalendar/index";
 import styles from "./page.module.scss";
@@ -13,24 +12,45 @@ import { useSelector } from "react-redux";
 import { useFetch } from "../../hooks/useFetch";
 
 export default function Home() {
-  
-  const userName = useSelector((state) => state.auth.user?.name) // 로그인된 사용자 정보(name)
-  const isAdmin = useSelector((state) => state.auth.user?.role) === "admin"
-  const { data: classData } = useFetch(`http://localhost:3000/api/classCalendar`);
+  const userName = useSelector((state) => state.auth.user?.name); // 로그인된 사용자 정보(name)
+  const isAdmin = useSelector((state) => state.auth.user?.role) === "admin";
+  const { data: mockClassData } = useFetch(`http://localhost:3000/api/classCalendar`);
   const [selectedFilter, setSelectedFilter] = useState(isAdmin ? "전체보기" : userName);
-  const [classListData, setClassListData] = useState(classData || []); // 수업 데이터
+  const [classListData, setClassListData] = useState(() => {
+    const storedData = localStorage.getItem("classData");
+    return storedData ? JSON.parse(storedData) : [];
+  }); // 수업 데이터
   const [showModal, setShowModal] = useState(false); // 수업 추가/수정 모달
   const [selectedClass, setSelectedClass] = useState(null); // 선택한 수업(admin)
 
   useEffect(() => {
-    if (classData) {
-      setClassListData(classData);
-    }
-  }, [classData]);
+    const handleStorageChange = () => {
+      const storedData = localStorage.getItem("classData");
+      if (storedData) {
+        setClassData(JSON.parse(storedData));
+      }
+    };
+    // 로컬 스토리지 변경 감지
+    window.addEventListener("storage", handleStorageChange);
+    // 컴포넌트 언마운트 시 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
 
- 
-  const getClassList = (classData, selectedFilter) => {
-    return selectedFilter === "전체보기" ? classData : classData.filter((item) => item.instructor === selectedFilter);
+  useEffect(() => {
+    if (!classListData && mockClassData) {
+      localStorage.setItem("classData", JSON.stringify(mockClassData)); // 데이터를 JSON 문자열로 변환하여 저장
+      setClassListData(mockClassData);
+    } else if (classListData) {
+      localStorage.setItem("classData", JSON.stringify(classListData)); // 데이터를 JSON 문자열로 변환하여 저장
+    }
+  }, [classListData, mockClassData]);
+
+  const getClassList = (classListData, selectedFilter) => {
+    return selectedFilter === "전체보기"
+      ? classListData
+      : classListData.filter((item) => item.instructor === selectedFilter);
   };
 
   // 수업 추가/수정
@@ -89,11 +109,7 @@ export default function Home() {
             <li>토요일</li>
             <li>일요일</li>
           </ul>
-          <CalendarDate
-            classList={filteredClassList}
-            onEdit={handleEditClass}
-            onDelete={handleDelete}
-          />
+          <CalendarDate classList={filteredClassList} onEdit={handleEditClass} onDelete={handleDelete} />
         </div>
 
         <AddClassModal
